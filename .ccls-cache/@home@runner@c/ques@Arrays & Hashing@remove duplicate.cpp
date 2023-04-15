d5069f5dc18ffb0da3843712ccc7ec463a4c6dde
HARI OM TRIPATHI <122279024+hari27om@users.noreply.github.com>
2023-04-15 11:11:37 +0000

    remove duplicate.

---
+++ .ccls-cache/@home@runner@c/ques@Arrays & Hashing@remove duplicate.cpp
@@ -0,0 +1,15 @@
+// remove duplicate in place from sorted array.(we have to modify the array)
+
+1. Approach
+  use a set to solve it.
+  // s.insert arr(i);
+  tc=(nlogn)
+
+2.Approach
+  // two pointer approach
+  i,j=a[0];
+  if a[j]!=a[i]
+  {
+    a[i+1]=a[j];
+    i++;
+}
