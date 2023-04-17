48e9e88d153477b39d38b54fb40d909cc795ef14
HARI OM TRIPATHI <122279024+hari27om@users.noreply.github.com>
2023-04-17 17:00:31 +0000

    find missing no.

---
+++ ques/Arrays & Hashing/linear search.cpp
@@ -0,0 +1,8 @@
+// find missing no btw 1 to n 
+1. Approach 
+    brute force linear search  
+2. Approach 
+    use map to check freq of each no from 1 to n
+3. Approach 
+    use summation tech.
+  n*(n+1)/2.
